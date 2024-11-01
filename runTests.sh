e957f18981fd85fd58d808c55abf0702a638753a
matth2k <mrh259@cornell.edu>
2024-11-01 15:24:11 -0400

    add basic verilog test

--- runTests.sh
+++ runTests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -exo pipefail
 
-cargo run --release tests/examples.txt -k 4 | FileCheck tests/examples.txt
+cargo run --release tests/lutlang/examples.txt -k 4 | FileCheck tests/lutlang/examples.txt
 # Run again but with proofs
-cargo run --release tests/examples.txt -k 4 -v | FileCheck tests/examples.txt
+cargo run --release tests/lutlang/examples.txt -k 4 -v | FileCheck tests/lutlang/examples.txt
+
+cargo run --release --bin parse-verilog -- tests/verilog/mux_4_1_synth.v 2>>/dev/null | FileCheck tests/verilog/mux_4_1_synth.v
+
+
 echo "SUCCESS"
