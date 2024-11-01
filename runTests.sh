dde3500e8c20416b6685e07a23e2b2910d48fa1f
matth2k <mrh259@cornell.edu>
2024-11-01 15:17:14 -0400

    add filecheck to CI

--- runTests.sh
+++ runTests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -exo pipefail
 
-cargo test
 cargo run --release tests/examples.txt -k 4 | FileCheck tests/examples.txt
 # Run again but with proofs
 cargo run --release tests/examples.txt -k 4 -v | FileCheck tests/examples.txt
