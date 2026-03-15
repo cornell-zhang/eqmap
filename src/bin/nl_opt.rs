5b3ea54fec1d240e0b7ef39812cc2adf1a91db37
matth2k <mrh259@cornell.edu>
2026-03-15 19:01:20 -0400

    Bump versions

--- src/bin/nl_opt.rs
+++ src/bin/nl_opt.rs
@@ -4,7 +4,8 @@ use eqmap::pass::{Error, Pass, PrintVerilog};
 use eqmap::register_passes;
 use eqmap::verilog::sv_parse_wrapper;
 use nl_compiler::{from_vast, from_vast_overrides};
-use safety_net::{Identifier, Instantiable, MultiDiGraph, Netlist, SimpleCombDepth, format_id};
+use safety_net::graph::{CombDepthInfo, MultiDiGraph};
+use safety_net::{Identifier, Instantiable, Netlist, format_id};
 use std::io::Read;
 use std::path::PathBuf;
 use std::rc::Rc;
@@ -137,7 +138,7 @@ pub struct ReportDepth;
 impl Pass for ReportDepth {
     type I = PrimitiveCell;
     fn run(&self, netlist: &Rc<Netlist<Self::I>>) -> Result<String, Error> {
-        let analysis = netlist.get_analysis::<SimpleCombDepth<_>>()?;
+        let analysis = netlist.get_analysis::<CombDepthInfo<_>>()?;
         match analysis.get_max_depth() {
             Some(depth) => Ok(format!("Maximum combinational depth: {depth}")),
             None => Ok("Maximum combinational depth: undefined".to_string()),
